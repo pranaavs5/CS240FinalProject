03435c28ab602e4921bbadef268890599f7eb126
Pranaav Krishna Srinivasan <pranaavk.srinivasan@gmail.com>
2025-05-07 12:50:23 -0700

    example programs

---
+++ ExampleProgram2.c
@@ -0,0 +1,12 @@
+int base;
+int exponent;
+int result;
+
+base = 2;
+exponent = 3;
+
+// Use the custom power instruction
+// This will calculate base^exponent
+result = 0; // Will be set by pwr instruction
+
+return 0;
---
+++ ExampleProgram3.c
@@ -0,0 +1,10 @@
+int base;
+int number;
+int result;
+
+base = 2;
+number = 8;
+
+result = 0; 
+
+return 0;
